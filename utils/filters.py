c0460834c7b779893a7789540fa730fc0c4a00d9
Sawyer Andrews <hi@sawyer.systems>
2026-07-09 21:41:45 -0500

    pre-compile shader regex in memory instead of re-compiling it every request

--- utils/filters.py
+++ utils/filters.py
@@ -1,4 +1,6 @@
 import re
 
+BLOCK_SHADE_RE = re.compile(r'([█▓▒░▄▀▌▐])')
+
 def block_shade_filter(text):
-    return re.sub(r'([█▓▒░▄▀▌▐])', r'<span class="block">\1</span>', text)
+    return BLOCK_SHADE_RE.sub(r'<span class="block">\1</span>', text)
